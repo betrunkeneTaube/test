accad08e4a5c3079672d5b766120e50ac99ff4d7
betrunkeneTaube <frupucsic@gmail.com>
2023-03-24 13:46:53 +0300

    added count20

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')
 
 
-# Press the green button in the gutter to run the script.
+def count20():
+    for i in range(21):
+        var = lambda a: a ** 3
+        i = var(i)
+        print(i)
+
+
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    count20()
